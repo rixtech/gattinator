2cfb7e7cc6cc21a8d26990416e0320154c57567a
Rick Teh <rickteh@melbpc.org.au>
2017-07-05 20:28:06 +1000

    Add a webservice user and test database

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -12,3 +12,32 @@ chown -R ubuntu:www-data /var/www
 
 apt-get install -y debconf-utils
 
+export DEBIAN_FRONTEND="noninteractive"
+
+PW=$(openssl rand -base64 12)
+
+cat<<End_PW>/root/.my.cnf
+[client]
+user=root
+password=$PW
+End_PW
+chmod 0400 /root/.my.cnf
+
+apt-get install debconf-utils
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PW"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PW"
+
+apt-get install -y mysql-server 
+
+# Add a webservice user and test database
+mysql<<EndSQL
+USE mysql;
+CREATE DATABASE testdb;
+CREATE USER webapp IDENTIFIED BY 'webappPW';
+GRANT ALL ON testdb.* TO 'webapp'@'%';
+FLUSH PRIVILEGES;
+USE testdb;
+CREATE table test (c CHAR(255));
+INSERT INTO test (c) VALUES ("hello world");
+EndSQL
+
